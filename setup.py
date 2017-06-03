34ec9b8cc890a0c97d5f8983da3630946748f633
Dax Mickelson <dmickels@cisco.com>
2017-06-03 10:27:56 -0600

    Trying to get the installer to work.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     ],
     keywords='fmcapi fmc ftd security cisco ngfw api',
     packages=find_packages(exclude=['docs', 'tests*']),
-    install_requires=['requests', 'datetime', 'json', 'time', 're', 'ipaddress'],
+    install_requires=['requests', 'datetime', 'time', 're', 'ipaddress'],
     package_data={},
     data_files=None,
 )
