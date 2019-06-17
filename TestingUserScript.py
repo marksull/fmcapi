46b8bfb055f945f4bf0c1ecbcb549329faaf14ca
parkerbrother1 <40617976+parkerbrother1@users.noreply.github.com>
2019-06-16 22:14:13 -0400

    Add PolicyAssignments Class.  Update TestingUserScript for AutoNatRules and PolicyAssignments classes

--- TestingUserScript.py
+++ TestingUserScript.py
@@ -940,6 +940,112 @@ def test__port_object_group():
     logging.info('# Testing PortObjectGroup class done.\n')
 
 
+def test__autonat():
+    logging.info('# Testing FTDNatPolicy class.')
+    # Create a container policy for FTD NAT rules
+    natpol1 = FTDNatPolicy(fmc=fmc1, name=namer)
+    natpol1.post()
+    natpol1.get()
+
+    # Create original and translate objects
+    obj1 = IPNetwork(fmc=fmc1)
+    obj1.name = 'net_original'
+    obj1.value = '10.0.0.0/8'
+    obj1.post()
+    time.sleep(1)
+
+    obj2 = IPHost(fmc=fmc1)
+    obj2.name = 'net_xlate'
+    obj2.value = '192.0.2.1'
+    obj2.post()
+    time.sleep(1)
+
+    # Create identity nat object
+    obj3 = IPNetwork(fmc=fmc1)
+    obj3.name = 'net_identity'
+    obj3.value = '192.168.0.0/24'
+    obj3.post()
+    time.sleep(1)
+
+    # Create nat pool objects
+    obj4 = IPNetwork(fmc=fmc1)
+    obj4.name = 'net_original_pool'
+    obj4.value = '172.16.0.0/24'
+    obj4.post()
+    time.sleep(1)
+
+    # PAT Pool must be a range, not a subnet
+    obj5 = IPRange(fmc=fmc1)
+    obj5.name = 'net_xlate_pool'
+    obj5.value = '192.0.2.128-192.0.2.254'
+    obj5.post()
+    time.sleep(1)
+
+    # Create interface PAT object
+    obj6 = IPNetwork(fmc=fmc1)
+    obj6.name = 'net_original_intf'
+    obj6.value = '192.168.1.0/24'
+    obj6.post()
+    time.sleep(1)
+
+    # Autonat a network object to a host
+    autonat1 = AutoNatRules(fmc=fmc1)
+    autonat1.original_network(name="net_original")
+    autonat1.translated_network(name="net_xlate")
+    autonat1.natType = "STATIC"
+    # Source and destination interface can be either an interface group or security zone
+    autonat1.source_intf(name="IG-INSIDE")
+    autonat1.destination_intf(name="SZ-OUTSIDE1")
+    autonat1.nat_policy(name=namer)
+
+    # Autonat identity nat
+    autonat2 = AutoNatRules(fmc=fmc1)
+    autonat2.identity_nat(name="net_identity")
+    autonat2.source_intf(name="IG-INSIDE")
+    autonat2.destination_intf(name="SZ-OUTSIDE1")
+    autonat2.nat_policy(name=namer)
+
+    # Autonat nat pool
+    autonat3 = AutoNatRules(fmc=fmc1)
+    autonat3.original_network(name="net_original_pool")
+    autonat3.patPool(name="net_xlate_pool")
+    autonat3.source_intf(name="IG-INSIDE")
+    autonat3.destination_intf(name="SZ-OUTSIDE1")
+    autonat3.nat_policy(name=namer)
+
+    # Autonat interface PAT
+    autonat4 = AutoNatRules(fmc=fmc1)
+    autonat4.original_network(name="net_original_intf")
+    autonat4.natType = "DYNAMIC"
+    autonat4.source_intf(name="IG-INSIDE")
+    autonat4.destination_intf(name="SZ-OUTSIDE1")
+    autonat4.nat_policy(name=namer)
+    autonat4.interfaceInTranslatedNetwork = True
+
+    autonat1.post()
+    autonat2.post()
+    autonat3.post()
+    autonat4.post()
+
+    # Associate a nat policy to a device
+    # Do not uncomment if you do not have a device registered to FPMC
+    # Use name of device or deviceHAPair as applicable
+    pol_devices = [{"name": "deviceHAName", "type": "deviceHAPair"}]
+    assign1 = PolicyAssignments(fmc=fmc1)
+    assign1.ftd_natpolicy(name=namer, devices=pol_devices)
+    assign1.post()
+
+    assign1.ftd_natpolicy(name=namer, devices=[])
+    assign1.put()
+    natpol1.delete()
+    obj1.delete()
+    obj2.delete()
+    obj3.delete()
+    obj4.delete()
+    obj5.delete()
+    obj6.delete()
+
+
 def wait_for_task(task, wait_time=10):
     try:
         status = TaskStatuses(
@@ -1009,6 +1115,7 @@ with FMC(host=host, username=username, password=password, autodeploy=autodeploy)
     test__access_control_policy()
     test__acp_rule()
     test__audit()
+    test__autonat()
     '''
     These tests require registered devices
     #test__device_with_task()
