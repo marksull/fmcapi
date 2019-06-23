7c774b3978bf4fdada050289b32c46abbb9688df
parkerbrother1 <40617976+parkerbrother1@users.noreply.github.com>
2019-06-22 21:46:32 -0400

    Add IKEv1 and IKEv2 Policy and Proposal Objects and unit testing

--- TestingUserScript.py
+++ TestingUserScript.py
@@ -409,7 +409,7 @@ def test__ip_range():
     logging.info('# Test IPRange done.\n')
 
 
-def test__icmpv4_objects():
+def test__icmpv4():
     logging.info(
     '# Test ICMPv4Object.  Post, get, put, delete ICMPv4Object Objects.')
     obj1 = ICMPv4Object(fmc=fmc1)
@@ -425,7 +425,7 @@ def test__icmpv4_objects():
     obj1.delete()
     logging.info('# FQDNS ICMPv4Object class done.\n')
 
-def test__icmpv6_objects():
+def test__icmpv6():
     logging.info(
     '# Test ICMPv6Object.  Post, get, put, delete ICMPv6Object Objects.')
     obj1 = ICMPv6Object(fmc=fmc1)
@@ -440,6 +440,107 @@ def test__icmpv6_objects():
 
     obj1.delete()
     logging.info('# FQDNS ICMPv6Object class done.\n')
+
+
+def test__ikev1():
+    logging.info(
+    '# Test IKEv1Policies and IKEv1IpsecProposals.  Post, get, put, delete IKEv1Policies and IKEv1IpsecProposals Objects.')
+    ipsec1 = IKEv1IpsecProposals(fmc=fmc1)
+    ipsec1.name = "_ipsec" + namer
+    ipsec1.espEncryption = "AES-128"
+    ipsec1.espHash = "SHA"
+    ipsec1.post()
+
+    ipsec1.get()
+    ipsec1.espEncryption = "AES-192"
+    ipsec1.put()
+
+    pol1 = IKEv1Policies(fmc=fmc1)
+    pol1.name = "_pol" + namer
+    pol1.encryption = "3DES"
+    pol1.hash = "SHA"
+    pol1.priority = "10"
+    pol1.diffieHellmanGroup = "5"
+    pol1.authenticationMethod = "Preshared Key"
+    pol1.lifetimeInSeconds = "3600"
+    pol1.post()
+
+    pol1.get()
+    pol1.encryption = "AES-128"
+    pol1.put()
+
+    ipsec1.delete()
+    pol1.delete()
+    logging.info('# Test IKEv1Policies and IKEv1IpsecProposals classes done.\n')
+
+
+def test__ikev2():
+    logging.info(
+    '# Test IKEv2Policies and IKEv2IpsecProposals.  Post, get, put, delete IKEv2Policies and IKEv2IpsecProposals Objects.')
+    encryption_list = ['AES', 'AES-192', 'AES-256', 'NULL']
+    integrity_list1 = ['NULL', 'SHA-1', 'SHA-256', 'SHA-384', 'SHA-512']
+    ipsec_integrity_list1 = ['NULL', 'SHA', 'SHA-256', 'SHA-384', 'SHA-512']
+    # 'NULL' is invalid for prf_integrity.  Should generate a warning log and ignore that type.
+    prf_integrity_list1 = ['NULL', 'SHA', 'SHA-256', 'SHA-384', 'SHA-512']
+    
+    ipsec1 = IKEv2IpsecProposals(fmc=fmc1)
+    ipsec1.name = "_ipsec" + namer
+    ipsec1.encryption(action='add', algorithms=encryption_list)
+    ipsec1.hash(action='add', algorithms=integrity_list1)
+    ipsec1.post()
+
+    ipsec1.get()
+    # Try to add a duplicate
+    ipsec1.encryption(action='add', algorithms=['AES-192'])
+    ipsec1.hash(action='add', algorithms=['SHA-1'])
+
+    ipsec1.encryption(action='remove', algorithms=['NULL'])
+    ipsec1.hash(action='remove', algorithms=['NULL'])
+    ipsec1.put()
+
+    # None of the algorithms can contain an empty list
+    ipsec1.get()
+    ipsec1.encryption(action='clear')
+    ipsec1.hash(action='clear')
+    ipsec1.encryption(action='add', algorithms=['AES-192'])
+    ipsec1.hash(action='add', algorithms=['SHA-1'])
+    ipsec1.put()
+    
+
+    pol1 = IKEv2Policies(fmc=fmc1)
+    pol1.name = "_pol" + namer
+    pol1.priority = "10"
+    pol1.diffieHellmanGroups = ["2", "5"]
+    pol1.encryption(action='add', algorithms=encryption_list)
+    pol1.hash(action='add', algorithms=ipsec_integrity_list1)
+    pol1.prf_hash(action='add', algorithms=prf_integrity_list1)
+    pol1.lifetimeInSeconds = "3600"
+    pol1.post()
+
+    pol1.get()
+    # Try to add a duplicate
+    pol1.encryption(action='add', algorithms=['AES-192'])
+    pol1.hash(action='add', algorithms=['NULL'])
+    pol1.prf_hash(action='add', algorithms=['SHA'])
+
+    pol1.encryption(action='remove', algorithms=['NULL'])
+    pol1.hash(action='remove', algorithms=['NULL'])
+    pol1.prf_hash(action='remove', algorithms=['SHA'])
+    pol1.put()
+
+    # None of the algorithms can contain an empty list
+    pol1.get()
+    pol1.encryption(action='clear')
+    pol1.hash(action='clear')
+    pol1.prf_hash(action='clear')
+    pol1.encryption(action='add', algorithms=['AES-192'])
+    pol1.hash(action='add', algorithms=['NULL'])
+    pol1.prf_hash(action='add', algorithms=['SHA'])
+    pol1.put()
+
+    ipsec1.delete()
+    pol1.delete()
+    logging.info('# Test IKEv2Policies and IKEv2IpsecProposals classes done.\n')
 
 
 def test__url():
@@ -1571,8 +1672,10 @@ with FMC(host=host, username=username, password=password, autodeploy=autodeploy)
     test__ip_host()
     test__ip_network()
     test__ip_range()
-    test__icmpv4_objects()
-    test__icmpv6_objects()
+    test__icmpv4()
+    test__icmpv6()
+    test__ikev1()
+    test__ikev2()
     test__url()
     test__vlan_tag()
     test__protocol_port()
--- fmcapi/api_objects.py
+++ fmcapi/api_objects.py
@@ -1702,6 +1702,335 @@ class ICMPv6Object(APIClassTemplate):
             self.overridable = kwargs['overridable']
 
 
+class IKEv1IpsecProposals(APIClassTemplate):
+    """
+    The IKEv1IpsecProposals Object in the FMC.
+    """
+
+    URL_SUFFIX = '/object/ikev1ipsecproposals'
+    REQUIRED_FOR_POST = ['name', 'espEncryption', 'espHash']
+    VALID_FOR_ENCRYPTION = ['DES', '3DES', 'AES-128', 'AES-192', 'AES-256', 'ESP-NULL']
+    VALID_FOR_HASH = ['NONE', 'MD5', 'SHA']
+    VALID_CHARACTERS_FOR_NAME = """[.\w\d_\- ]"""
+
+    def __init__(self, fmc, **kwargs):
+        super().__init__(fmc, **kwargs)
+        logging.debug("In __init__() for IKEv1IpsecProposals class.")
+        self.parse_kwargs(**kwargs)
+        self.type = 'IKEv1IPsecProposal'
+
+    def format_data(self):
+        logging.debug("In format_data() for IKEv1IpsecProposals class.")
+        json_data = {}
+        if 'id' in self.__dict__:
+            json_data['id'] = self.id
+        if 'name' in self.__dict__:
+            json_data['name'] = self.name
+        if 'type' in self.__dict__:
+            json_data['type'] = self.type
+        if 'espEncryption' in self.__dict__:
+            json_data['espEncryption'] = self.espEncryption
+        if 'espHash' in self.__dict__:
+            json_data['espHash'] = self.espHash
+        return json_data
+
+    def parse_kwargs(self, **kwargs):
+        super().parse_kwargs(**kwargs)
+        logging.debug("In parse_kwargs() for IKEv1IpsecProposals class.")
+        if 'espEncryption' in kwargs:
+            self.espEncryption = kwargs['espEncryption']
+        if 'espHash' in kwargs:
+            self.espHash = kwargs['espHash']
+
+
+class IKEv1Policies(APIClassTemplate):
+    """
+    The IKEv1Policies Object in the FMC.
+    """
+
+    URL_SUFFIX = '/object/ikev1policies'
+    REQUIRED_FOR_POST = ['name', 'encryption', 'hash', 'diffieHellmanGroup', 'lifetimeInSeconds', 'authenticationMethod']
+    VALID_FOR_ENCRYPTION = ['DES', '3DES', 'AES-128', 'AES-192', 'AES-256']
+    VALID_FOR_HASH = ['MD5', 'SHA']
+    VALID_CHARACTERS_FOR_NAME = """[.\w\d_\- ]"""
+
+    def __init__(self, fmc, **kwargs):
+        super().__init__(fmc, **kwargs)
+        logging.debug("In __init__() for IKEv1Policies class.")
+        self.parse_kwargs(**kwargs)
+        self.type = 'Ikev1PolicyObject'
+
+    def format_data(self):
+        logging.debug("In format_data() for IKEv1Policies class.")
+        json_data = {}
+        if 'id' in self.__dict__:
+            json_data['id'] = self.id
+        if 'name' in self.__dict__:
+            json_data['name'] = self.name
+        if 'type' in self.__dict__:
+            json_data['type'] = self.type
+        if 'encryption' in self.__dict__:
+            if self.encryption in self.VALID_FOR_ENCRYPTION:
+                json_data['encryption'] = self.encryption
+            else:
+                logging.warning('encryption {} not a valid type".'.format(self.encryption))
+        if 'hash' in self.__dict__:
+            if self.hash in self.VALID_FOR_HASH:
+                json_data['hash'] = self.hash
+            else:
+                logging.warning('hash {} not a valid type".'.format(self.hash))
+        if 'priority' in self.__dict__:
+            json_data['priority'] = self.priority
+        if 'diffieHellmanGroup' in self.__dict__:
+            json_data['diffieHellmanGroup'] = self.diffieHellmanGroup
+        if 'authenticationMethod' in self.__dict__:
+            json_data['authenticationMethod'] = self.authenticationMethod
+        if 'lifetimeInSeconds' in self.__dict__:
+            json_data['lifetimeInSeconds'] = self.lifetimeInSeconds
+        return json_data
+
+    def parse_kwargs(self, **kwargs):
+        super().parse_kwargs(**kwargs)
+        logging.debug("In parse_kwargs() for IKEv1Policies class.")
+        if 'encryption' in kwargs:
+            self.encryption = kwargs['encryption']
+        if 'hash' in kwargs:
+            self.hash = kwargs['hash']
+        if 'priority' in kwargs:
+            self.priority = kwargs['priority']
+        if 'diffieHellmanGroup' in kwargs:
+            self.diffieHellmanGroup = kwargs['diffieHellmanGroup']
+        if 'authenticationMethod' in kwargs:
+            self.authenticationMethod = kwargs['authenticationMethod']
+        if 'lifetimeInSeconds' in kwargs:
+            self.lifetimeInSeconds = kwargs['lifetimeInSeconds']
+
+
+class IKEv2IpsecProposals(APIClassTemplate):
+    """
+    The IKEv2IpsecProposals Object in the FMC.
+    """
+
+    URL_SUFFIX = '/object/ikev2ipsecproposals'
+    REQUIRED_FOR_POST = ['name', 'encryptionAlgorithms', 'integrityAlgorithms']
+    VALID_FOR_ENCRYPTION = ['DES', '3DES', 'AES', 'AES-192', 'AES-256', 'NULL', 'AES-GCM', 'AES-GCM-192', 'AES-GCM-256', 'AES-GMAC', 'AES-GMAC-192', 'AES-GMAC-256']
+    VALID_FOR_HASH = ['NULL', 'MD5', 'SHA-1', 'SHA-256', 'SHA-384', 'SHA-512']
+    VALID_CHARACTERS_FOR_NAME = """[.\w\d_\- ]"""
+
+    def __init__(self, fmc, **kwargs):
+        super().__init__(fmc, **kwargs)
+        logging.debug("In __init__() for IKEv2IpsecProposals class.")
+        self.parse_kwargs(**kwargs)
+        self.type = 'IKEv2IPsecProposal'
+
+    def format_data(self):
+        logging.debug("In format_data() for IKEv2IpsecProposals class.")
+        json_data = {}
+        if 'id' in self.__dict__:
+            json_data['id'] = self.id
+        if 'name' in self.__dict__:
+            json_data['name'] = self.name
+        if 'type' in self.__dict__:
+            json_data['type'] = self.type
+        if 'encryptionAlgorithms' in self.__dict__:
+            json_data['encryptionAlgorithms'] = self.encryptionAlgorithms
+        if 'integrityAlgorithms' in self.__dict__:
+            json_data['integrityAlgorithms'] = self.integrityAlgorithms
+        return json_data
+
+    def parse_kwargs(self, **kwargs):
+        super().parse_kwargs(**kwargs)
+        logging.debug("In parse_kwargs() for IKEv2IpsecProposals class.")
+        if 'encryptionAlgorithms' in kwargs:
+            self.encryptionAlgorithms = kwargs['encryptionAlgorithms']
+        if 'integrityAlgorithms' in kwargs:
+            self.integrityAlgorithms = kwargs['integrityAlgorithms']
+
+    def encryption(self, action, algorithms=[]):
+        logging.debug("In encryption() for IKEv2IpsecProposals class.")
+        if action == 'add':
+            for algorithm in algorithms:
+                if 'encryptionAlgorithms' in self.__dict__:
+                        if algorithm in self.encryptionAlgorithms:
+                            logging.warning('encryptionAlgorithms {} already exists".'.format(algorithm))
+                        elif algorithm in self.VALID_FOR_ENCRYPTION:
+                            self.encryptionAlgorithms.append(algorithm)
+                        else:
+                            logging.warning('encryptionAlgorithms {} not a valid type".'.format(algorithm))
+                else:
+                    self.encryptionAlgorithms = [algorithm]
+        elif action == 'remove':
+            if 'encryptionAlgorithms' in self.__dict__:
+                for algorithm in algorithms:
+                    self.encryptionAlgorithms = list(filter(lambda i: i != algorithm, self.encryptionAlgorithms))
+            else:
+                logging.warning('IKEv2IpsecProposals has no members.  Cannot remove encryptionAlgorithms.')
+        elif action == 'clear':
+            if 'encryptionAlgorithms' in self.__dict__:
+                del self.encryptionAlgorithms
+                logging.info('All encryptionAlgorithms removed from this IKEv2IpsecProposals object.')
+
+    def hash(self, action, algorithms=[]):
+        logging.debug("In hash() for IKEv2IpsecProposals class.")
+        if action == 'add':
+            for algorithm in algorithms:
+                if 'integrityAlgorithms' in self.__dict__:
+                        if algorithm in self.integrityAlgorithms:
+                            logging.warning('integrityAlgorithms {} already exists".'.format(algorithm))
+                        elif algorithm in self.VALID_FOR_HASH:
+                            self.integrityAlgorithms.append(algorithm)
+                        else:
+                            logging.warning('integrityAlgorithms {} not a valid type".'.format(algorithm))
+                else:
+                    self.integrityAlgorithms = [algorithm]
+        elif action == 'remove':
+            if 'integrityAlgorithms' in self.__dict__:
+                for algorithm in algorithms:
+                    self.integrityAlgorithms = list(filter(lambda i: i != algorithm, self.integrityAlgorithms))
+            else:
+                logging.warning('IKEv2IpsecProposals has no members.  Cannot remove integrityAlgorithms.')
+        elif action == 'clear':
+            if 'integrityAlgorithms' in self.__dict__:
+                del self.integrityAlgorithms
+                logging.info('All integrityAlgorithms removed from this IKEv2IpsecProposals object.')
+
+
+class IKEv2Policies(APIClassTemplate):
+    """
+    The IKEv2Policies Object in the FMC.
+    """
+
+    URL_SUFFIX = '/object/ikev2policies'
+    REQUIRED_FOR_POST = ['name', 'integrityAlgorithms', 'prfIntegrityAlgorithms', 'encryptionAlgorithms', 'diffieHellmanGroups']
+    VALID_FOR_ENCRYPTION = ['DES', '3DES', 'AES', 'AES-192', 'AES-256', 'NULL', 'AES-GCM', 'AES-GCM-192', 'AES-GCM-256']
+    VALID_FOR_INTEGRITY = ['NULL', 'MD5', 'SHA', 'SHA-256', 'SHA-384', 'SHA-512']
+    VALID_FOR_PRF_INTEGRITY = ['MD5', 'SHA', 'SHA-256', 'SHA-384', 'SHA-512']
+    VALID_CHARACTERS_FOR_NAME = """[.\w\d_\- ]"""
+
+    def __init__(self, fmc, **kwargs):
+        super().__init__(fmc, **kwargs)
+        logging.debug("In __init__() for IKEv2Policies class.")
+        self.parse_kwargs(**kwargs)
+        self.type = 'Ikev2PolicyObject'
+
+    def format_data(self):
+        logging.debug("In format_data() for IKEv2Policies class.")
+        json_data = {}
+        if 'id' in self.__dict__:
+            json_data['id'] = self.id
+        if 'name' in self.__dict__:
+            json_data['name'] = self.name
+        if 'type' in self.__dict__:
+            json_data['type'] = self.type
+        if 'priority' in self.__dict__:
+            json_data['priority'] = self.priority
+        if 'diffieHellmanGroups' in self.__dict__:
+            json_data['diffieHellmanGroups'] = self.diffieHellmanGroups
+        if 'integrityAlgorithms' in self.__dict__:
+            json_data['integrityAlgorithms'] = self.integrityAlgorithms
+        if 'prfIntegrityAlgorithms' in self.__dict__:
+            json_data['prfIntegrityAlgorithms'] = self.prfIntegrityAlgorithms
+        if 'encryptionAlgorithms' in self.__dict__:
+            json_data['encryptionAlgorithms'] = self.encryptionAlgorithms
+        if 'lifetimeInSeconds' in self.__dict__:
+            json_data['lifetimeInSeconds'] = self.lifetimeInSeconds
+        return json_data
+
+    def parse_kwargs(self, **kwargs):
+        super().parse_kwargs(**kwargs)
+        logging.debug("In parse_kwargs() for IKEv2Policies class.")
+        if 'priority' in kwargs:
+            self.priority = kwargs['priority']
+        if 'diffieHellmanGroups' in kwargs:
+            self.diffieHellmanGroups = kwargs['diffieHellmanGroups']
+        if 'integrityAlgorithms' in kwargs:
+            self.integrityAlgorithms = kwargs['integrityAlgorithms']
+        if 'prfIntegrityAlgorithms' in kwargs:
+            self.prfIntegrityAlgorithms = kwargs['prfIntegrityAlgorithms']
+        if 'encryptionAlgorithms' in kwargs:
+            self.encryptionAlgorithms = kwargs['encryptionAlgorithms']
+        if 'lifetimeInSeconds' in kwargs:
+            self.lifetimeInSeconds = kwargs['lifetimeInSeconds']
+
+    def encryption(self, action, algorithms=[]):
+        logging.debug("In encryption() for IKEv2Policies class.")
+        if action == 'add':
+            for algorithm in algorithms:
+                if 'encryptionAlgorithms' in self.__dict__:
+                        if algorithm in self.encryptionAlgorithms:
+                            logging.warning('encryptionAlgorithms {} already exists".'.format(algorithm))
+                        elif algorithm in self.VALID_FOR_ENCRYPTION:
+                            self.encryptionAlgorithms.append(algorithm)
+                        else:
+                            logging.warning('encryptionAlgorithms {} not a valid type".'.format(algorithm))
+                else:
+                    self.encryptionAlgorithms = [algorithm]
+        elif action == 'remove':
+            if 'encryptionAlgorithms' in self.__dict__:
+                for algorithm in algorithms:
+                    self.encryptionAlgorithms = list(filter(lambda i: i != algorithm, self.encryptionAlgorithms))
+            else:
+                logging.warning('IKEv2Policies has no members.  Cannot remove encryptionAlgorithms.')
+        elif action == 'clear':
+            if 'encryptionAlgorithms' in self.__dict__:
+                del self.encryptionAlgorithms
+                logging.info('All encryptionAlgorithms removed from this IKEv2Policies object.')
+
+    def hash(self, action, algorithms=[]):
+        logging.debug("In hash() for IKEv2Policies class.")
+        if action == 'add':
+            for algorithm in algorithms:
+                if 'integrityAlgorithms' in self.__dict__:
+                        if algorithm in self.integrityAlgorithms:
+                            logging.warning('integrityAlgorithms {} already exists".'.format(algorithm))
+                        elif algorithm in self.VALID_FOR_INTEGRITY:
+                            self.integrityAlgorithms.append(algorithm)
+                        else:
+                            logging.warning('integrityAlgorithms {} not a valid type".'.format(algorithm))
+                else:
+                    if algorithm in self.VALID_FOR_INTEGRITY:
+                        self.integrityAlgorithms = [algorithm]
+                    else:
+                        logging.warning('integrityAlgorithms {} not a valid type".'.format(algorithm))
+        elif action == 'remove':
+            if 'integrityAlgorithms' in self.__dict__:
+                for algorithm in algorithms:
+                    self.integrityAlgorithms = list(filter(lambda i: i != algorithm, self.integrityAlgorithms))
+            else:
+                logging.warning('IKEv2Policies has no members.  Cannot remove integrityAlgorithms.')
+        elif action == 'clear':
+            if 'integrityAlgorithms' in self.__dict__:
+                del self.integrityAlgorithms
+                logging.info('All integrityAlgorithms removed from this IKEv2Policies object.')
+
+    def prf_hash(self, action, algorithms=[]):
+        logging.debug("In prf_hash() for IKEv2Policies class.")
+        if action == 'add':
+            for algorithm in algorithms:
+                if 'prfIntegrityAlgorithms' in self.__dict__:
+                        if algorithm in self.prfIntegrityAlgorithms:
+                            logging.warning('prfIntegrityAlgorithms {} already exists".'.format(algorithm))
+                        elif algorithm in self.VALID_FOR_PRF_INTEGRITY:
+                            self.prfIntegrityAlgorithms.append(algorithm)
+                        else:
+                            logging.warning('prfIntegrityAlgorithms {} not a valid type".'.format(algorithm))
+                else:
+                    if algorithm in self.VALID_FOR_PRF_INTEGRITY:
+                        self.prfIntegrityAlgorithms = [algorithm]
+                    else:
+                      logging.warning('prfIntegrityAlgorithms {} not a valid type".'.format(algorithm))  
+        elif action == 'remove':
+            if 'prfIntegrityAlgorithms' in self.__dict__:
+                for algorithm in algorithms:
+                    self.prfIntegrityAlgorithms = list(filter(lambda i: i != algorithm, self.prfIntegrityAlgorithms))
+            else:
+                logging.warning('IKEv2Policies has no members.  Cannot remove prfIntegrityAlgorithms.')
+        elif action == 'clear':
+            if 'prfIntegrityAlgorithms' in self.__dict__:
+                del self.prfIntegrityAlgorithms
+                logging.info('All prfIntegrityAlgorithms removed from this IKEv2Policies object.')
+
+
 class PortObjectGroup(APIClassTemplate):
     """
     The PortObjectGroup Object in the FMC.
