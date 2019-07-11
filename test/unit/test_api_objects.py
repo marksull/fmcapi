e58b4fac772448c7d2cb494a42cd7ea35986d1e2
Shakti Kumar <shakti.shrivastava13@gmail.com>
2019-07-11 21:36:22 +0530

    test case for no objects present initially

--- test/unit/test_api_objects.py
+++ test/unit/test_api_objects.py
@@ -122,7 +122,7 @@ class TestApiObjects(unittest.TestCase):
     @mock.patch('fmcapi.api_objects.NetworkGroup')
     @mock.patch('fmcapi.api_objects.FQDNS')
     @mock.patch('fmcapi.api_objects.IPAddresses')
-    def test_ACPRule_source_network_with_for_objects_and_no_objects_initially(self, mock_ipaddress, mock_fqdns,
+    def test_ACPRule_source_network_for_objects_and_no_objects_initially(self, mock_ipaddress, mock_fqdns,
                                                                               mock_nwgroup, _):
         value2 = mock.Mock()
         value = mock.Mock()
