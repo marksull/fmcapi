6bc43bca5a503ac8ae4b6bfe27e92faa81515ac8
Dax Mickelson <dmickels@cisco.com>
2019-07-11 17:39:53 -0600

    re-enable autodeploy after Device registration.  However, my PC clock is ahead of the FMC so deploy doesn't work.

--- example/hq-ftd.py
+++ example/hq-ftd.py
@@ -87,6 +87,8 @@ def main():
         hq_ftd.post()
         # At the moment fmcapi doesn't have good support for waiting for the device registration process to complete.
         time.sleep(300)
+        # The Device Class disables the fmc.autodeploy.  We are waiting for the registration, we can re-enable.
+        hq_fmc.autodeploy = True
 
         # Once registration is complete configure the interfaces of hq-ftd.
         hq_ftd_g00 = PhysicalInterface(fmc=hq_fmc, device_name=hq_ftd.name)
