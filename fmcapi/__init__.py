aa712fdc92751f3bbe38c0a764de40389e84c020
Dax Mickelson <dmickels@cisco.com>
2019-08-19 08:26:42 -0600

    prep for release

--- fmcapi/__init__.py
+++ fmcapi/__init__.py
@@ -12,7 +12,8 @@ logging.debug("In the fmcapi __init__.py file.")
 def __authorship__():
     """In the FMC __authorship__() class method:
 ***********************************************************************************************************************
-This python module was created by Dax Mickelson along with LOTs of help from Ryan Malloy and Neil Patel.
+This python module was created by Dax Mickelson along with LOTs of help from Ryan Malloy and Neil Patel.  Thank you to
+the github community members who have also pitched in, especially Mark Sullivan and his team.
 Feel free to send me comments/suggestions/improvements.  Either by email: dmickels@cisco.com or more importantly
 via a Pull request from the github repository: https://github.com/daxm/fmcapi.
 ***********************************************************************************************************************
