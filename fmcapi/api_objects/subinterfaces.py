399bd71b47bba79e8802ec1f925e5d5f5753c1e7
Dax Mickelson <dmickels@cisco.com>
2019-09-08 09:53:18 -0600

    f-stringed

--- fmcapi/api_objects/subinterfaces.py
+++ fmcapi/api_objects/subinterfaces.py
@@ -82,14 +82,14 @@ class SubInterfaces(APIClassTemplate):
             if list(kwargs['ipv4'].keys())[0] in self.VALID_FOR_IPV4:
                 self.ipv4 = kwargs['ipv4']
             else:
-                logging.warning('Method {} is not a valid ipv4 type.'.format(kwargs['ipv4']))
+                logging.warning(f"""Method "{kwargs['ipv4']}" is not a valid ipv4 type.""")
         if 'device_name' in kwargs:
             self.device(device_name=kwargs['device_name'])
         if 'mode' in kwargs:
             if kwargs['mode'] in self.VALID_FOR_MODE:
                 self.mode = kwargs['mode']
             else:
-                logging.warning('Mode {} is not a valid mode.'.format(kwargs['mode']))
+                logging.warning(f"""Mode "{kwargs['mode']}" is not a valid mode.""")
         if 'securityZone' in kwargs:
             self.securityZone = kwargs['securityZone']
         if 'enabled' in kwargs:
@@ -98,7 +98,7 @@ class SubInterfaces(APIClassTemplate):
             if kwargs['MTU'] in self.VALID_FOR_MTU:
                 self.MTU = kwargs['MTU']
             else:
-                logging.warning('MTU {} should be in the range 64-9000".'.format(kwargs['MTU']))
+                logging.warning(f"""MTU "{kwargs['MTU']}" should be in the range 64-9000.""")
                 self.MTU = 1500
         if 'managementOnly' in kwargs:
             self.managementOnly = kwargs['managementOnly']
@@ -135,10 +135,10 @@ class SubInterfaces(APIClassTemplate):
         device1.get(name=device_name)
         if 'id' in device1.__dict__:
             self.device_id = device1.id
-            self.URL = '{}{}/{}/subinterfaces'.format(self.fmc.configuration_url, self.PREFIX_URL, self.device_id)
+            self.URL = f'{self.fmc.configuration_url}{self.PREFIX_URL}/{self.device_id}/subinterfaces'
             self.device_added_to_url = True
         else:
-            logging.warning('Device {} not found.  Cannot set up device for SubInterfaces.'.format(device_name))
+            logging.warning(f'Device "{device_name}" not found.  Cannot set up device for SubInterfaces.')
 
     def sz(self, name):
         logging.debug("In sz() for SubInterfaces class.")
@@ -148,7 +148,7 @@ class SubInterfaces(APIClassTemplate):
             new_zone = {'name': sz.name, 'id': sz.id, 'type': sz.type}
             self.securityZone = new_zone
         else:
-            logging.warning('Security Zone, "{}", not found.  Cannot add to SubInterfaces.'.format(name))
+            logging.warning(f'Security Zone, "{name}", not found.  Cannot add to SubInterfaces.')
 
     def static(self, ipv4addr, ipv4mask):
         logging.debug("In static() for SubInterfaces class.")
@@ -167,4 +167,4 @@ class SubInterfaces(APIClassTemplate):
             if 'MTU' not in self.__dict__:
                 self.MTU = intf1.MTU
         else:
-            logging.warning('PhysicalInterface, "{}", not found.  Cannot add to SubInterfaces.'.format(intf1.name))
+            logging.warning(f'PhysicalInterface, "{intf1.name}", not found.  Cannot add to SubInterfaces.')
