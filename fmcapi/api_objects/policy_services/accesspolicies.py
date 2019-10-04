58119eb613a4f0a8be82dd8629f720d2ab50b4cf
Dax Mickelson <dmickels@cisco.com>
2019-10-04 14:25:54 -0600

    defaultAction checks now enforced.

--- fmcapi/api_objects/policy_services/accesspolicies.py
+++ fmcapi/api_objects/policy_services/accesspolicies.py
@@ -13,26 +13,40 @@ class AccessPolicies(APIClassTemplate):
     URL_SUFFIX = '/policy/accesspolicies'
     REQUIRED_FOR_POST = ['name']
     REQUIRED_FOR_PUT = ['id', 'defaultAction']
-    DEFAULT_ACTION_OPTIONS = ['BLOCK', 'NETWORK_DISCOVERY', 'IPS']  # Not implemented yet.
+    DEFAULT_ACTION_OPTIONS = ['BLOCK', 'TRUST', 'PERMIT', 'NETWORK_DISCOVERY', 'INHERIT_FROM_PARENT']
     FILTER_BY_NAME = True
+
+    @property
+    def defaultAction(self):
+        return {'action': self._defaultAction}
+
+    @defaultAction.setter
+    def defaultAction(self, action):
+        if action in self.DEFAULT_ACTION_OPTIONS:
+            self._defaultAction = action
+        else:
+            logging.error(f'action, {action}, is not a valid option.  Choose from {self.DEFAULT_ACTION_OPTIONS}.')
 
     def __init__(self, fmc, **kwargs):
         super().__init__(fmc, **kwargs)
         logging.debug("In __init__() for AccessPolicies class.")
         self.parse_kwargs(**kwargs)
-        self._defaultAction = {'action': 'BLOCK'}
+        self._defaultAction = None
+        self.defaultAction = 'BLOCK'
 
-    def parse_kwargs(self, **kwargs):
-        super().parse_kwargs(**kwargs)
-        logging.debug("In parse_kwargs() for AccessPolicies class.")
+    def format_data(self):
+        json_data = super().format_data()
+        logging.debug("In format_data() for AccessPolicies class.")
+        json_data['defaultAction'] = self.defaultAction
+        return json_data
 
-    @property
-    def defaultAction(self):
-        return self._defaultAction
+    def put(self):
+        logging.info('PUT method for API for AccessPolicies not supported.')
+        pass
 
-    @defaultAction.setter
-    def defaultAction(self, action):
-        self._defaultAction = {'action': action}
+    def delete(self):
+        logging.info('DELETE method for API for AccessPolicies not supported.')
+        pass
 
 
 class AccessControlPolicy(AccessPolicies):
