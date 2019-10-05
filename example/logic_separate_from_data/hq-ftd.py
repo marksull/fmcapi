6409a7ed38b09932226038e263adf3be41c7a600
Dax Mickelson <dmickels@cisco.com>
2019-10-05 07:34:10 -0600

    Built nat policy code

--- example/logic_separate_from_data/hq-ftd.py
+++ example/logic_separate_from_data/hq-ftd.py
@@ -56,41 +56,89 @@ def create_access_policies(fmc, acp_list):
         policy = fmcapi.AccessPolicies(fmc=fmc, name=acp['name'], defaultAction=acp['default_action'])
         policy.post()
         # Build access_rules associated with this acp.
-        for rule in acp['access_rules']:
-            acp_rule = fmcapi.AccessRules(fmc=fmc, acp_name=policy.name, name=rule['name'])
-            """  This is broken at the moment.
-            if 'log_begin' in rule:
-                acp_rule.logBegin = rule['log_begin']
-            if 'log_end' in rule:
-                acp_rule.logBegin = rule['log_end']
-            """
-            if 'action' in rule:
-                acp_rule.action = rule['action']
-            if 'source_networks' in rule:
-                for sn in rule['source_networks']:
-                    acp_rule.source_network(action='add', name=sn['name'])
-            if 'destination_networks' in rule:
-                for dn in rule['destination_networks']:
-                    acp_rule.destination_network(action='add', name=dn['name'])
-            if 'source_ports' in rule:
-                for sp in rule['source_ports']:
-                    acp_rule.source_port(action='add', name=sp['name'])
-            if 'destination_ports' in rule:
-                for dp in rule['destination_ports']:
-                    acp_rule.destination_port(action='add', name=dp['name'])
-            if 'intrusion_policy' in rule:
-                acp_rule.intrusion_policy(action='add', name=rule['intrusion_policy'])
-            """
-            # Using SGTs isn't implemented in fmcapi yet.
-            if 'source_ise_sgts' in rule:
-                for sgt in rule['source_ise_sgts']:
-                    acp_rule.source_ise_sgt(action='add', name=sgt['name'])
-            if 'destination_ise_sgts' in rule:
-                for sgt in rule['destination_ise_sgts']:
-                    acp_rule.destiantion_ise_sgt(action='add', name=sgt['name'])
-            """
-            acp_rule.post()
-            del acp_rule
+        if 'rules' in acp:
+            for rule in acp['rules']:
+                acp_rule = fmcapi.AccessRules(fmc=fmc, acp_name=policy.name, name=rule['name'])
+                """  This is broken at the moment.
+                if 'log_begin' in rule:
+                    acp_rule.logBegin = rule['log_begin']
+                if 'log_end' in rule:
+                    acp_rule.logBegin = rule['log_end']
+                """
+                if 'enabled' in rule:
+                    acp_rule.enabled = rule['enabled']
+                if 'action' in rule:
+                    acp_rule.action = rule['action']
+                if 'source_networks' in rule:
+                    for sn in rule['source_networks']:
+                        acp_rule.source_network(action='add', name=sn['name'])
+                if 'destination_networks' in rule:
+                    for dn in rule['destination_networks']:
+                        acp_rule.destination_network(action='add', name=dn['name'])
+                if 'source_ports' in rule:
+                    for sp in rule['source_ports']:
+                        acp_rule.source_port(action='add', name=sp['name'])
+                if 'destination_ports' in rule:
+                    for dp in rule['destination_ports']:
+                        acp_rule.destination_port(action='add', name=dp['name'])
+                if 'intrusion_policy' in rule:
+                    acp_rule.intrusion_policy(action='add', name=rule['intrusion_policy'])
+                """
+                # Using SGTs isn't implemented in fmcapi yet.
+                if 'source_ise_sgts' in rule:
+                    for sgt in rule['source_ise_sgts']:
+                        acp_rule.source_ise_sgt(action='add', name=sgt['name'])
+                if 'destination_ise_sgts' in rule:
+                    for sgt in rule['destination_ise_sgts']:
+                        acp_rule.destination_ise_sgt(action='add', name=sgt['name'])
+                """
+                acp_rule.post()
+                del acp_rule
+        del policy
+
+
+def create_nat_policies(fmc, nat_list):
+    """Create Nat Policies and their AccessRules"""
+    for natp in nat_list:
+        policy = fmcapi.FTDNatPolicies(fmc=fmc, name=natp['name'])
+        policy.post()
+        # Build nat_rules associated with this acp.
+        if 'rules' in natp:
+            for rule in natp['rules']:
+                if 'auto' in rule:
+                    rule_data = rule['auto']
+                    autonat = fmcapi.AutoNatRules(fmc=fmc)
+                    if 'nat_type' in rule_data:
+                        autonat.natType = rule_data['nat_type']
+                    if 'interface_in_translated_network' in rule_data:
+                        autonat.interfaceInTranslatedNetwork = rule_data['interface_in_translated_network']
+                    if 'original_network' in rule_data:
+                        autonat.original_network(rule_data['original_network'])
+                    if 'source_interface' in rule_data:
+                        autonat.source_intf(name=rule_data['source_interface'])
+                    if 'destination_interface' in rule_data:
+                        autonat.destination_intf(name=rule_data['destination_interface'])
+                    autonat.nat_policy(name=natp['name'])
+                    autonat.post()
+                    del autonat
+                if 'manual' in rule:
+                    rule_data = rule['manual']
+                    manualnat = fmcapi.ManualNatRules(fmc=fmc)
+                    if 'nat_type' in rule_data:
+                        manualnat.natType = rule_data['nat_type']
+                    if 'original_source' in rule_data:
+                        manualnat.original_source(rule_data['original_source'])
+                    if 'translated_source' in rule_data:
+                        manualnat.translated_source(rule_data['translated_source'])
+                    if 'source_interface' in rule_data:
+                        manualnat.source_intf(name=rule_data['source_interface'])
+                    if 'destination_interface' in rule_data:
+                        manualnat.destination_intf(name=rule_data['destination_interface'])
+                    if 'enabled' in rule_data:
+                        manualnat.enabled = rule_data['enabled']
+                    manualnat.nat_policy(name=natp['name'])
+                    manualnat.post()
+                    del manualnat
         del policy
 
 
@@ -107,6 +155,7 @@ def create_device_records(fmc, device_list):
         # Push to FMC to start device registration.
         ftd.post(post_wait_time=dr['wait_for_post'])
         # Registration done.  Time to configure it's interfaces.
+        #  FIXME:  Doesn't process beyond this point for some reason.
         for interface_types in dr['interfaces']:
             if 'physical' in interface_types:
                 for interface in interface_types['physical']:
@@ -156,10 +205,13 @@ def program_fmc(data_vars):
                     create_networks(fmc=fmc1, network_list=data_vars['networks'])
                 if 'access_policies' in data_vars:
                     create_access_policies(fmc=fmc1, acp_list=data_vars['access_policies'])
+                if 'nat_policies' in data_vars:
+                    create_nat_policies(fmc=fmc1, nat_list=data_vars['nat_policies'])
                 if 'device_records' in data_vars:
                     create_device_records(fmc=fmc1, device_list=data_vars['device_records'])
         except:
-            logging.error(f"Section 'fmc' does not have enough/right information to establish a connection to FMC:")
+            logging.error(f"Section 'fmc' does not have the right information (bad password?)"
+                          f" to establish a connection to FMC:")
     else:
         logging.warning(f"No 'fmc' section found in {YAML_FILE}")
 
