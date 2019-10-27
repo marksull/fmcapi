1f5e05d23e15c66abed810c7ccf3d47abecef7a9
Dax Mickelson <dmickels@cisco.com>
2019-10-27 13:17:47 -0600

    Add more options for filter_query logic.

--- fmcapi/api_objects/apiclasstemplate.py
+++ fmcapi/api_objects/apiclasstemplate.py
@@ -48,6 +48,8 @@ class APIClassTemplate(object):
         json_data = {}
         filter_list = self.VALID_JSON_DATA
         if filter_query == 'all':
+            filter_list = self.__dict__
+        elif filter_query == 'kwargs':
             filter_list = self.VALID_FOR_KWARGS
         for key_value in filter_list:
             if key_value in self.__dict__:
