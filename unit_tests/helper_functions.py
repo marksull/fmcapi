451b2c0ba28785279e1fced507833ed16a9df63a
Dax Mickelson <dmickels@cisco.com>
2019-10-27 09:50:58 -0600

    Adding id_generator() for making random character strings.

---
+++ unit_tests/helper_functions.py
@@ -0,0 +1,8 @@
+"""Methods to assist making unit_tests"""
+
+import string
+import random
+
+
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
