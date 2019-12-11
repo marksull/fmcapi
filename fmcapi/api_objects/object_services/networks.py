351250c1ff11dc360c66028d1f841ab80cd20df0
Dax Mickelson <dmickels@cisco.com>
2019-12-11 13:50:28 -0700

    Moved "value" assignment out of __init__() and into parse_kwargs(). Updated Docstrings.

--- fmcapi/api_objects/object_services/networks.py
+++ fmcapi/api_objects/object_services/networks.py
@@ -1,3 +1,5 @@
+"""Networks Class."""
+
 from fmcapi.api_objects.apiclasstemplate import APIClassTemplate
 from fmcapi.api_objects.helper_functions import *
 import logging
@@ -5,9 +7,7 @@ import warnings
 
 
 class Networks(APIClassTemplate):
-    """
-    The Networks Object in the FMC.
-    """
+    """The Networks Object in the FMC."""
 
     VALID_JSON_DATA = ["id", "name", "value", "description"]
     VALID_FOR_KWARGS = VALID_JSON_DATA + []
@@ -15,9 +15,25 @@ class Networks(APIClassTemplate):
     REQUIRED_FOR_POST = ["name", "value"]
 
     def __init__(self, fmc, **kwargs):
+        """
+        Initialize Networks object.
+
+        :param fmc: (object) FMC object
+        :param kwargs: Any other values passed during instantiation.
+        :return: None
+        """
         super().__init__(fmc, **kwargs)
         logging.debug("In __init__() for Networks class.")
         self.parse_kwargs(**kwargs)
+
+    def parse_kwargs(self, **kwargs):
+        """
+        Parse the kwargs and set self variables to match.
+
+        :return: None
+        """
+        super().parse_kwargs(**kwargs)
+        logging.debug("In parse_kwargs() for AccessRules class.")
         if "value" in kwargs:
             value_type = get_networkaddress_type(kwargs["value"])
             if value_type == "range" or value_type == "host":
@@ -34,7 +50,11 @@ class Networks(APIClassTemplate):
 
 
 class IPNetwork(Networks):
-    """Dispose of this Class after 20210101."""
+    """
+    Dispose of this Class after 20210101.
+
+    Use Networks() instead.
+    """
 
     def __init__(self, fmc, **kwargs):
         warnings.resetwarnings()
