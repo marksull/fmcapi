b34d32e74fc92bf0ece10683d2a5942cbc4b0435
Dax Mickelson <dmickels@cisco.com>
2019-12-11 13:17:33 -0700

    Moved "value" assignment out of __init__() and into parse_kwargs(). Updated Docstrings.

--- fmcapi/api_objects/object_services/hosts.py
+++ fmcapi/api_objects/object_services/hosts.py
@@ -1,3 +1,5 @@
+"""Hosts Class."""
+
 from fmcapi.api_objects.apiclasstemplate import APIClassTemplate
 from fmcapi.api_objects.helper_functions import *
 import logging
@@ -5,9 +7,7 @@ import warnings
 
 
 class Hosts(APIClassTemplate):
-    """
-    The Host Object in the FMC.
-    """
+    """The Host Object in the FMC."""
 
     VALID_JSON_DATA = ["id", "name", "type", "value", "description"]
     VALID_FOR_KWARGS = VALID_JSON_DATA + []
@@ -16,9 +16,25 @@ class Hosts(APIClassTemplate):
     REQUIRED_FOR_PUT = ["id", "name", "value"]
 
     def __init__(self, fmc, **kwargs):
+        """
+        Initialize Hosts object.
+
+        :param fmc: (object) FMC object
+        :param kwargs: Any other values passed during instantiation.
+        :return: None
+        """
         super().__init__(fmc, **kwargs)
         logging.debug("In __init__() for IPHost class.")
         self.parse_kwargs(**kwargs)
+
+    def parse_kwargs(self, **kwargs):
+        """
+        Parse the kwargs and set self variables to match.
+
+        :return: None
+        """
+        super().parse_kwargs(**kwargs)
+        logging.debug("In parse_kwargs() for Hosts class.")
         if "value" in kwargs:
             value_type = get_networkaddress_type(kwargs["value"])
             if value_type == "range" or value_type == "network":
@@ -35,7 +51,11 @@ class Hosts(APIClassTemplate):
 
 
 class IPHost(Hosts):
-    """Dispose of this Class after 20210101."""
+    """
+    Dispose of this Class after 20210101.
+
+    Use Hosts() instead.
+    """
 
     def __init__(self, fmc, **kwargs):
         warnings.resetwarnings()
