83fabd4ff1bff4bc2ce2906dc8b2ee5ff8cca3e4
Dax Mickelson <dmickels@cisco.com>
2020-01-30 12:05:48 -0700

    Improving Docker menu logic

--- Docker/bootstrap.py
+++ Docker/bootstrap.py
@@ -27,7 +27,7 @@ if list_of_scripts:
     qty_of_scripts = len(list_of_scripts)
     try:
         choice = int(input(f'Select 0-{qty_of_scripts - 1}: '))
-        if 0 <= choice <= qty_of_scripts - 1:
+        if 0 <= choice < qty_of_scripts - 1:
             # A valid choice has been made.
             os.system(f"/usr/bin/python3 {os.path.join(script_dir, list_of_scripts[choice])}")
     except ValueError:
