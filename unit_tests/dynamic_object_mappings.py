080af28bd2b483027cd18fcff460ee65ea905fb4
amrnagar <amrnagar@cisco.com>
2024-01-09 14:06:20 +0530

    mappings get url different

--- unit_tests/dynamic_object_mappings.py
+++ unit_tests/dynamic_object_mappings.py
@@ -9,9 +9,9 @@ def test__dynamic_objects_mappings(fmc):
     # obj1.handle_mappings(action="add", value=["10.0.0.4"], name="Sales")
     # obj1.post()
 
-    obj2 = fmcapi.DynamicObjectMappings(fmc=fmc, id="0050568E-B2F8-0ed3-0000-519691059888")
-    obj2.mappings = ["70.0.0.0/24"]
-    logging.info(f"Dynamic object mapping {obj2.put()}")
+    obj2 = fmcapi.DynamicObjectMappings(fmc=fmc, id="0050568E-B2F8-0ed3-0000-523986013212")
+    #obj2.mappings = ["70.0.0.0/24"]
+    logging.info(f"Dynamic object mapping {obj2.get()}")
 
 
 
