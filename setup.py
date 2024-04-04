f432406f9b6f42645fa91a4862c5ff2682e2c7a7
zlantz <zlantz@liberty.edu>
2024-04-03 21:17:46 -0400

    ensure xmltodict is required for pip install

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     ],
     keywords="fmcapi fmc ftd security cisco ngfw api firepower",
     packages=find_packages(exclude=["docs", "tests*"]),
-    install_requires=["requests", "datetime", "ipaddress"],
+    install_requires=["requests", "datetime", "ipaddress", "xmltodict"],
     python_requires=">=3.6",
     package_data={},
     data_files=None,
