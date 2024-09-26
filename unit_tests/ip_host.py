9e796b505697439155971d5539445c1614fafd56
Zachery Lantz <zlantz@liberty.edu>
2024-09-26 15:26:09 -0400

    handling for bulk post and delete of objects

--- unit_tests/ip_host.py
+++ unit_tests/ip_host.py
@@ -1,7 +1,7 @@
 import logging
 import fmcapi
 import time
-
+from .helper_functions import id_generator
 
 def test__ip_host(fmc):
     logging.info("Test IPHost.  Post, get, put, delete Host Objects.")
@@ -15,11 +15,40 @@ def test__ip_host(fmc):
     obj1.post()
     time.sleep(1)
     del obj1
+
     obj1 = fmcapi.Hosts(fmc=fmc, name=namer)
     obj1.get()
     obj1.value = "9.9.9.9"
     obj1.put()
     time.sleep(1)
     obj1.delete()
+    del obj1
+
+    obj = fmcapi.Hosts(fmc=fmc)
+    obj.bulk = []
+    for i in range(50):
+        obj.bulk.append(
+            {
+                "name" : f"_fmcapi_test_{id_generator()}",
+                "value" : "9.9.9.9"
+            }
+        )
+    obj.bulk_post()
+
+    # sleep a bit otherwise the api just doesn't know that these objects are unused immediately after post
+    time.sleep(10)
+
+    ids = []
+    host_obs = fmcapi.Hosts(fmc=fmc)
+    unused_hosts = host_obs.get(unusedOnly=True)
+    if 'items' in unused_hosts:
+        for host in unused_hosts['items']:
+            ids.append(host['id'])
+
+        bulk_delete = fmcapi.Hosts(fmc=fmc)
+        bulk_delete.bulk = ids
+        bulk_delete.bulk_delete()
+    else:
+        logging.info(f'No unused objects to bulk delete')
 
     logging.info("Test IPHost done.\n")
